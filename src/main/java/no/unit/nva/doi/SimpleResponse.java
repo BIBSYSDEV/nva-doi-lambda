c8587c13275c25b5721b2830c58723d37e16c9f8
svenngjeto <svenn.gjeto@unit.no>
2020-01-03 15:10:29 +0100

    Returning data in POJO without statusCode

--- src/main/java/no/unit/nva/doi/SimpleResponse.java
+++ src/main/java/no/unit/nva/doi/SimpleResponse.java
@@ -3,17 +3,10 @@ package no.unit.nva.doi;
 public class SimpleResponse {
 
     private String body;
-    private int statusCode;
 
-
-    public SimpleResponse(String body, int statusCode) {
-        this.body = body;
-        this.statusCode = statusCode;
-    }
 
     public SimpleResponse(String payload) {
         this.body = payload;
-        this.statusCode = 200;
     }
 
     public String getBody() {
